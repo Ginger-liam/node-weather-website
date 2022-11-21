06b59da01ac934c0fbcda3cfa02c50b56bd4d5d7
Liam Willis <liam.willis@enorm.com>
2022-11-21 11:34:37 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,3 @@
-// fetch("https://puzzle.mead.io/puzzle").then((response) => {
-//   response.json().then((data) => {
-//     console.log(data);
-//   });
-// });
-
-// fetch("http://localhost:3000/weather?address=Dinantstraat").then((response) => {
-//   response.json().then((data) => {
-//     if (data.error) {
-//       console.log(data.error);
-//     } else {
-//       console.log(data.location);
-//       console.log(forecast);
-//     }
-//   });
-// });
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
@@ -28,19 +11,17 @@ weatherForm.addEventListener("submit", (e) => {
 
   messageOne.textContent = "Loading";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-          console.log(data.error);
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-          console.log(data.location);
-          console.log(data.forecast);
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        console.log(data.error);
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+  });
 });
